57d56005dbc3b76b5e5dedb6f83cb814c0651b51
anlien <anlien@xin.com>
2020-05-15 11:32:09 +0800

    ‘添加监听

--- command/start.js
+++ command/start.js
@@ -6,6 +6,7 @@ const path = require('path');
 const buildClient = require('./buildClient').default;
 const { runBuildServer } = require('./buildServer');
 const { clientConfig } = require('./config');
+const { compiling } = require('./libs/babel');
 
 function gzipSize(str) {
     return zlib.gzipSync(str, { level: 9 }).length;
@@ -47,10 +48,10 @@ function printFileSizesAfterBuild(webpackStats, buildFolder, isGzipSize) {
     });
 }
 
-function startNode(){
+function startNode() {
     console.log(process.cwd());
     const { spawn } = require('child_process');
-    const ls = spawn('node',['./dist/server/server/index.js']);
+    const ls = spawn('node', ['./dist/server/server/index.js']);
 
     ls.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
@@ -68,30 +69,37 @@ function startNode(){
 
 console.time('runBuildClient');
 
-buildClient().then(({stats,compilerClient}) => {
+buildClient().then(({ stats, compilerClient }) => {
     console.timeEnd('runBuildClient');
     console.log('File sizes:\n');
     printFileSizesAfterBuild(stats, clientConfig.buildDir, false);
     // console.log('File sizes after gzip:\n');
     // printFileSizesAfterBuild(stats, resolveApp('dist/www'), true);
+
     console.log('编译server端代码');
     runBuildServer();
-
+    console.log('启动node服务');
     startNode();
-
+    console.log('webpack 继续监听');
     webpackWatch(compilerClient);
-    setTimeout(()=>{ console.log('12151') },8000)
 });
 
 
-function webpackWatch(compilerClient){
-       //监听事件
+function webpackWatch(compilerClient) {
+    //监听事件
     compilerClient.watch({
         ignored: /node_modules/,
         aggregateTimeou: 300,
         pull: 1000  // 单位：ms
-    },(err,stats)=>{
-        console.log(Object.keys(stats));
+    }, (err, stats) => {
+        const { fileTimestamps } = stats.compilation;
+        if (fileTimestamps.size > 0) {
+            let timesTamps = Array.from(fileTimestamps);
+            timesTamps.sort((a, b) => b[1] - a[1]);
+            const [filePath, changeTime] = timesTamps[0];
+            console.log('webpack 编译：',filePath);
+            compiling([filePath]);
+        }
     });
 }
 
